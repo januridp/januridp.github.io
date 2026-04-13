68873229fea0fda7c15f44213fa0876de9b5a2a9
bizharedev <janury.geek@gmail.com>
2026-04-13 21:20:15 +0700

    fix: update ipstack API endpoint to use HTTPS for secure data fetching

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ function getUserAgentInfo() {
     const browser = /Edge\/\d+/.test(ua) ? 'Microsoft Edge' : /MSIE 9/.test(ua) ? 'Internet Explorer 9' : /MSIE 10/.test(ua) ? 'Internet Explorer 10' : /MSIE 11/.test(ua) ? 'Internet Explorer 11' : /MSIE\s\d/.test(ua) ? 'Internet Explorer?' : /rv\:11/.test(ua) ? 'Internet Explorer 11' : /Firefox\W\d/.test(ua) ? 'Firefox' : /Chrom(e|ium)\W\d|CriOS\W\d/.test(ua) ? 'Google Chrome' : /\bSafari\W\d/.test(ua) ? 'Safari' : /\bOpera\W\d/.test(ua) ? 'Opera' : /\bOPR\W\d/i.test(ua) ? 'Opera' : 'Other';
     const os = /Windows NT 10/.test(ua) ? "Windows 10" : /Windows NT 6\.0/.test(ua) ? "Windows Vista" : /Windows NT 6\.1/.test(ua) ? "Windows 7" : /Windows NT 6\.\d/.test(ua) ? "Windows 8" : /Windows NT 5\.1/.test(ua) ? "Windows XP" : /Windows NT [1-5]\./.test(ua) ? "Windows NT" : /Mac/.test(ua) ? "macOS" : /Linux/.test(ua) ? "Linux" : "Other OS";
     const mobile = /IEMobile|Windows Phone|Lumia/i.test(ua) ? 'Windows Phone' : /iPhone|iP[oa]d/.test(ua) ? 'iPhone' : /Android/.test(ua) ? 'Android' : /Mobile|Tablet/i.test(ua) ? 'Mobile/Tablet' : 'Desktop';
-    
+
     document.getElementById('ua-info').innerHTML = `
         <div class="prompt">Browser: <span style="color:var(--text-main)">${browser}</span></div>
         <div class="prompt">OS: <span style="color:var(--text-main)">${os}</span></div>
@@ -59,9 +59,9 @@ function getUserAgentInfo() {
 
 async function getIpInfo() {
     try {
-        const response = await fetch('http://api.ipstack.com/check?access_key=745f78a7cc0dc3c5b323e239b3e28547');
+        const response = await fetch('https://api.ipstack.com/check?access_key=745f78a7cc0dc3c5b323e239b3e28547');
         const data = await response.json();
-        
+
         document.getElementById('ip-info').innerHTML = `
             <div class="prompt">IP: <span style="color:var(--neon-cyan)">${data.ip || 'Unknown'}</span></div>
             <div class="prompt">Location: <span style="color:var(--text-main)">${data.city || ''}, ${data.country_name || 'Unknown'} <img src="${data.location?.country_flag || ''}" width="16" style="vertical-align: middle"></span></div>
